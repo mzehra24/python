1af88f3c7993f1e8592db94d3f026a912e7ff195
mzehra24 <154894760+mzehra24@users.noreply.github.com>
2025-03-26 15:22:57 +0530

    magic8.py
    The Magic 8 Ball is a popular office toy and children's toy invented in the 1940s for fortune-telling and advice seeking. 🎱
    
    It's an oversized 8 ball with some of the following answers:
    
    Yes - definitely.
    It is decidedly so.
    Without a doubt.
    Reply hazy, try again.
    Ask again later.
    Better not tell you now.
    My sources say no.
    Outlook not so good.
    Very doubtful.
    Create a magic8.py program that can respond to any Yes or No questions with a different answer each time it executes.

---
+++ magic8.py
@@ -0,0 +1,22 @@
+# Write code below 💖
+import random
+question = input('Ask a Question:')
+random_num = random.randint(1,9)
+if random_num == 1:
+  print("Yes - definitely.")
+elif random_num == 2:
+  print("It is decidedly so.")
+elif random_num == 3:
+  print("without a doubt.")
+elif random_num == 4:
+  print("Reply hazy, try again.")
+elif random_num == 5:
+  print("Ask again later.")
+elif random_num == 6:
+  print("Better not tell you now.")
+elif random_num == 7:
+  print("My sources say no.")
+elif random_num == 8:
+  print("Outlook not so good.")
+else:
+  print("very doubtful.")
