5e571c8ad5656b44b2b08f4fa90e4dafb279d4d2
mzehra24 <154894760+mzehra24@users.noreply.github.com>
2025-05-26 14:39:41 +0530

    list.py
    write a program to store seven fruits in a list entered by the user

---
+++ list.py
@@ -0,0 +1,9 @@
+fruit1=input("Enter 1st fruit:")
+fruit2=input("Enter 2nd fruit:")
+fruit3=input("Enter 3rd fruit:")
+fruit4=input("Enter 4th fruit:")
+fruit5=input("Enter 5th fruit:")
+fruit6=input("Enter 6th fruit:")
+fruit7=input("Enter 7th fruit:")
+fruits=fruit1,fruit2,fruit3,fruit4,fruit5,fruit6,fruit7
+print(list(fruits))
