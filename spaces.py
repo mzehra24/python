679370b5669de98e36605cc08e8fe8db03ce786d
mzehra24 <154894760+mzehra24@users.noreply.github.com>
2025-05-26 14:11:15 +0530

    spaces.py
    Write a python program to detect double spaces in the string.Replace double spaces with single space using above string.

---
+++ spaces.py
@@ -0,0 +1,10 @@
+# first method to detect spaces in the string
+e = "he  y"
+e.count(" ")
+
+#second method to detect spaces in the string
+e = "he  y"
+e.find(" ")
+
+#Replacing double spaces in the string by single space
+e.replace("  "," ")
