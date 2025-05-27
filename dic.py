215d833ca57b96339af459eaa084a63aad613ed3
mzehra24 <154894760+mzehra24@users.noreply.github.com>
2025-05-27 16:50:37 +0530

    dic.py
    write a python program to create a dictionary with hindi words as keys and english translation of hindi words as values take user input.

---
+++ dic.py
@@ -0,0 +1,10 @@
+key1=input("Enter key1:")
+key2=input("Enter key2:")
+key3=input("Enter key3:")
+keys=key1,key2,key3
+val1=input("Enter val1:")
+val2=input("Enter val2:")
+val3=input("Enter val3:")
+vals=val1,val2,val3
+d=dict(zip(keys,vals))
+print(d)
