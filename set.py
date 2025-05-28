8d59762e1bbf40b5db14fdaa24cd097e02fed1a7
mzehra24 <154894760+mzehra24@users.noreply.github.com>
2025-05-28 13:58:07 +0530

    set.py
    write a program to input 8 numbers from the user and display all the unique numbers once.

---
+++ set.py
@@ -0,0 +1,11 @@
+num1=int(input("Enter num1:"))
+num2=int(input("Enter num2:"))
+num3=int(input("Enter num3:"))
+num4=int(input("Enter num4:"))
+num5=int(input("Enter num5:"))
+num6=int(input("Enter num6:"))
+num7=int(input("Enter num7:"))
+num8=int(input("Enter num8:"))
+nums=num1,num2,num3,num4,num5,num6,num7,num8
+res=set(nums)
+print(res)
