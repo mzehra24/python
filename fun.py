9b193b8189eaac0e5229649917713974199cbd5e
mzehra24 <154894760+mzehra24@users.noreply.github.com>
2025-05-29 16:20:59 +0530

    fun.py
    write a python program to calculate geometric mean with finding greatest among the two numbers a and b similarly calculate for c and d

---
+++ fun.py
@@ -0,0 +1,16 @@
+def calculateGmean(a,b):
+  mean=(a*b)/(a+b)
+  print(mean)
+a=9
+b=8
+def isGreater(a,b):
+  if (a>b):
+    print("First number is greater")
+  else:
+    print("Second number is greater")
+calculateGmean(a,b)
+isGreater(a,b)
+c=8
+d=75
+calculateGmean(c,d)
+isGreater(c,d)
