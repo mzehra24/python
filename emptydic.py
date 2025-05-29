8cec77bb8c79e08ea7c188c3e6225ff23b872a07
mzehra24 <154894760+mzehra24@users.noreply.github.com>
2025-05-29 18:02:32 +0530

    emptydic.py
    write a python program to create an empty dictionary and allow 4 friends to enter their names as keys and values as favourite language and fill the dictionary

---
+++ emptydic.py
@@ -0,0 +1,14 @@
+d={}
+name1=input("Enter your name:")
+name2=input("Enter your name:")
+name3=input("Enter your name:")
+name4=input("Enter your name:")
+f1=input("Enter fav lang:")
+f2=input("Enter fav lang:")
+f3=input("Enter fav lang:")
+f4=input("Enter fav lang:")
+d[name1]=f1
+d[name2]=f2
+d[name3]=f3
+d[name4]=f4
+print(d)
