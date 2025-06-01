037c6f9a42f4dff8e701d801b608afcf07031925
mzehra24 <154894760+mzehra24@users.noreply.github.com>
2025-06-01 13:27:46 +0530

    spam.py
    A spam comment is defined as a text containing following keywords:
    "make a lot of money" ,"buy now","subscribe this","click this".Write a program to detect these spams

---
+++ spam.py
@@ -0,0 +1,5 @@
+spam ="make a lot of money" ,"buy now","subscribe this","click this"
+if "make alot if money" or "buy now"or"subscribe this"or"click this" in spam:
+  print("This is spam")
+else:
+  print("This is not spam")
